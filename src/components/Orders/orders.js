213471b0e4b21ad56c9ca10b5a46876ee368504c
rajnikantsethiya <rajnikant.sethiya@gmail.com>
2017-05-26 04:51:10 +0530

    Update orders.js

--- src/components/Orders/orders.js
+++ src/components/Orders/orders.js
@@ -1,9 +1,9 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
-import ordersJSON from '../../config/orders.js';
+import ordersJSON from '../../data/orders.js';
 import SearchInput, { createFilter } from 'react-search-input';
 
-const KEYS_TO_FILTERS = ['id']
+const KEYS_TO_FILTERS = ['id'];
 class Orders extends Component {
   constructor(props) {
     super(props);
@@ -15,22 +15,25 @@ class Orders extends Component {
     this.searchUpdated = this.searchUpdated.bind(this);
   }
 
-  searchUpdated (term) {
-    this.setState({searchTerm: term})
+  searchUpdated(term) {
+    this.setState({ searchTerm: term });
   }
 
   render() {
     const { order } = this.props;
     return (
       <div>
-      <SearchInput className="search-input" onChange={this.searchUpdated} />
+        <div className="search-block">
+          <h2 className="search-heading">Search by Id : </h2>
+          <SearchInput className="search-input" onChange={this.searchUpdated} />
+        </div><br/>
       {
         ordersJSON.orders && ordersJSON.orders.map((item, key) => {
-          const isMatch = (item.outlet_id === order.id) ? true : false;          
+          const isMatch = (item.outlet_id === order.id) ? true : false;
           const filteredOrder = item.Orderdetail.filter(createFilter(this.state.searchTerm, KEYS_TO_FILTERS));
           if (isMatch) {
             return (
-                <table key={key}>
+                <table cellPadding="10" key={key}>
                   <tbody>
                     <tr className="table-row">
                       <th>ID</th>
@@ -70,3 +73,4 @@ function mapStateToProps(state) {
 }
 
 export default connect(mapStateToProps)(Orders);
+
